ce28b6aa686f2a21125a32c33e2ad10c7768cc8b
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2020-12-08 13:43:54 +0100

    NEW: PermissionHandler instalado y corriendo en iOS y Android. Ya tenemos botón para pedir la localización y tenemos control sobre lo que ocurre si tenemos o no acceso a la localización.

--- lib/pages/accesoGps.dart
+++ lib/pages/accesoGps.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class AccesoGpsPage extends StatelessWidget {
+class AccesoGpsPage extends StatefulWidget {
+  @override
+  _AccesoGpsPageState createState() => _AccesoGpsPageState();
+}
+
+class _AccesoGpsPageState extends State<AccesoGpsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +24,32 @@ class AccesoGpsPage extends StatelessWidget {
               shape: StadiumBorder(),
               elevation: 0,
               splashColor: Colors.transparent,
-              onPressed: () {},
+              onPressed: () async {
+                final status = await Permission.location.request();
+                print(status);
+                this.accesoGps(status);
+              },
             ),
           ],
         ),
       ),
     );
+  }
 
-    void accesoGps() {}
+  void accesoGps(PermissionStatus status) {
+    switch (status) {
+      case PermissionStatus.undetermined:
+        // Debería tener siempre status, puede ser que le salga el permiso y cierre la app
+        break;
+      case PermissionStatus.granted:
+        // Si es un statefull hay acceso dentro del state al context sin necesidad alguna
+        Navigator.pushReplacementNamed(context, 'mapa');
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.restricted:
+      case PermissionStatus.permanentlyDenied:
+        openAppSettings();
+        break;
+    }
   }
 }
