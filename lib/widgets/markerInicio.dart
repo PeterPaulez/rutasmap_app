b346d676ebe71fccbf9a0f2b33440ce5030d08c1
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2020-12-18 11:07:05 +0100

    NEW: Creamos un Painter de texto para poder pintar textos en nuestro canvas, lo hacemos con un método puesto que son 3 textos con valores y parámetros muy parecidos.

--- lib/widgets/markerInicio.dart
+++ lib/widgets/markerInicio.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+/*
+Con el custom painter podermo dibujar lo que queramos.
+Todo funcina como capa encima de otra en el canvas:
+Lo primero estará en el fondo y lo último estara arriba del todo.
+*/
 class MarkerInicioPainter extends CustomPainter {
+  final int minutos;
+  MarkerInicioPainter(this.minutos);
+
   @override
   void paint(Canvas canvas, Size size) {
     // Lapicero Negro
@@ -23,6 +31,45 @@ class MarkerInicioPainter extends CustomPainter {
       circuloBlancoRadio,
       paint,
     );
+
+    // Sombra
+    final Path path = new Path();
+    path.moveTo(40, 20);
+    path.lineTo(size.width - 10, 20);
+    path.lineTo(size.width - 10, 100);
+    path.lineTo(40, 100);
+    canvas.drawShadow(
+      path,
+      Colors.black87,
+      10,
+      false,
+    );
+
+    // Caja Blanca
+    final cajaBlanca = Rect.fromLTWH(40, 20, size.width - 55, 80);
+    canvas.drawRect(cajaBlanca, paint);
+
+    // Caja Negra
+    paint.color = Colors.black;
+    final cajaNegra = Rect.fromLTWH(40, 20, 70, 80);
+    canvas.drawRect(cajaNegra, paint);
+
+    // Dibujar Tiempo
+    TextPainter textPainter;
+    textPainter = _drawTest('$minutos', 30);
+    textPainter.paint(canvas, Offset(40, 35));
+    textPainter = _drawTest('Min', 20);
+    textPainter.paint(canvas, Offset(40, 65));
+
+    // Dibujar Ubicación
+    textPainter = _drawTest(
+      'Mi Ubicación',
+      22,
+      color: Colors.black,
+      minWidth: 0,
+      maxWidth: size.width - 130,
+    );
+    textPainter.paint(canvas, Offset(130, 50));
   }
 
   @override
@@ -30,4 +77,32 @@ class MarkerInicioPainter extends CustomPainter {
 
   @override
   bool shouldRebuildSemantics(MarkerInicioPainter oldDelegate) => false;
+
+  TextPainter _drawTest(
+    String text,
+    double size, {
+    Color color = Colors.white,
+    double maxWidth = 70,
+    double minWidth = 70,
+  }) {
+    TextSpan textSpan = new TextSpan(
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+      ),
+      text: text,
+    );
+    final textPainter = new TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );
+    textPainter.layout(
+      maxWidth: maxWidth,
+      minWidth: minWidth,
+    );
+
+    return textPainter;
+  }
 }
